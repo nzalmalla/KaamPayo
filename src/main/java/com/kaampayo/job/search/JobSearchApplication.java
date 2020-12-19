4f4fb168bddeef835cb118a7507933d6ca6585f8
nzalmalla <medenimmalla@gmail.com>
2020-12-19 12:20:35 -0500

    search jobdec15

--- src/main/java/com/kaampayo/job/search/JobSearchApplication.java
+++ src/main/java/com/kaampayo/job/search/JobSearchApplication.java
@@ -2,9 +2,10 @@ package com.kaampayo.job.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JobSearchApplication {
+public class JobSearchApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobSearchApplication.class, args);
---
+++ src/main/java/com/kaampayo/job/search/features/user/MvcConfig.java
@@ -0,0 +1,14 @@
+package com.kaampayo.job.search.features.user;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+    }
+
+}
