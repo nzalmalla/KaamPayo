cc98039180d53b019138a1ea56fc9026a5a68f0e
kshitijd <dhakalkshitij@gmail.com>
2020-12-23 20:01:06 +0545

    bug fixes

--- src/main/java/com/kaampayo/job/search/features/user/AccountController.java
+++ src/main/java/com/kaampayo/job/search/features/user/AccountController.java
@@ -1,41 +1,45 @@
 package com.kaampayo.job.search.features.user;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.logging.Logger;
 
 @Controller
 public class AccountController {
 
-    private  static final Logger log = (Logger) LoggerFactory.getLogger(AccountController.class);
+    private static final Logger log = LoggerFactory.getLogger(AccountController.class);
 
-@InitBinder
-public void initBinder(WebDataBinder dataBinder){
-    StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
-    dataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
-}
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("register")
-    public String register(@ModelAttribute UserDTO userDTO, Model model){
+    public String register(@ModelAttribute UserDTO userDTO, Model model) {
         model.addAttribute("userDTO", userDTO);
         return "register";
     }
+
     @PostMapping("/register")
     public String save(@Valid
-                                   UserDTO userDTO, BindingResult bindingResult){
+                               UserDTO userDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "register";
         }
         log.info(
                 userDTO.toString());
-return "redirect:/login";
+        return "redirect:/login";
 
     }
 }
